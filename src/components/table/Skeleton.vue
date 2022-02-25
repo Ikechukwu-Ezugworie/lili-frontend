116a4bf33db7f380fca1ec3188a2028816695dad
Jroid <i.ezugworie@gmail.com>
2022-02-25 11:05:52 +0100

    chore: add table component

---
+++ src/components/table/Skeleton.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="shadow p-4 w-full mx-auto">
+    <div class="animate-pulse flex space-x-4">
+      <div v-for="i in repeat" :key="i" class="flex-1 space-y-6 py-1">
+        <div class="h-4 bg-gray-300 rounded w-3/4" />
+        <div class="space-y-6">
+          <div class="h-4 bg-gray-400 rounded" />
+          <div class="h-4 bg-gray-400 rounded w-5/6" />
+          <div class="h-4 bg-gray-400 rounded w-3/4" />
+          <div class="h-4 bg-gray-400 rounded w-5/6" />
+          <div class="h-4 bg-gray-400 rounded w-3/4" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  props: {
+    repeat: {
+      type: Number,
+      default: 1,
+    }
+  }
+})
+</script>
---
+++ src/components/table/index.ts
@@ -0,0 +1,4 @@
+import Skeleton from './Skeleton.vue'
+import Pagination from './Pagination.vue'
+
+export { Skeleton, Pagination, }
