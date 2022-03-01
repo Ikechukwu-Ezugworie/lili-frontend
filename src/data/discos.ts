860782ef167e75d4ceee0737107851d1d42cbeb1
Jroid <i.ezugworie@gmail.com>
2022-03-01 02:03:22 +0100

    fix: update disco logos

--- src/data/discos.ts
+++ src/data/discos.ts
@@ -1,9 +1,9 @@
 import { ref } from 'vue'
-import { aedc, eedc, ekedc, ibedc, ikeja, jedc, kedc, kedco, phedc } from '../assets/disco_logos'
+import { aedc, bedc, eedc, ekedc, ibedc, ikeja, jedc, kedc, kedco, phedc, yedc } from '../assets/disco_logos'
 
 export const discos = ref([
   { label: 'AEDC (Abuja)', value: 1, logo: aedc },
-  { label: 'BEDC (Benin)', value: 2, logo: aedc },
+  { label: 'BEDC (Benin)', value: 2, logo: bedc },
   { label: 'EEDC (Enugu)', value: 3, logo: eedc },
   { label: 'EEDC (Eko)', value: 4, logo: ekedc },
   { label: 'IBEDC (Ibadan)', value: 5, logo: ibedc },
@@ -12,5 +12,5 @@ export const discos = ref([
   { label: 'KEDC (Kaduna)', value: 8, logo: kedc },
   { label: 'KEDCO (Kano)', value: 9, logo: kedco },
   { label: 'PHEDC (Port-harcourt)', value: 10, logo: phedc },
-  { label: 'YEDC (Yola)', value: 11, logo: aedc },
+  { label: 'YEDC (Yola)', value: 11, logo: yedc },
 ])
