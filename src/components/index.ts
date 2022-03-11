a9bc74e3290fadfedb7d01ceebf8048aef92df59
Ikechukwu Ezugworie <ikechukwue@hardcorebiometric.com>
2022-03-11 02:20:18 +0100

    chore: added validation to purchase form

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,7 +2,8 @@ import Login from './Login.vue'
 import Purchase from './Purchase.vue'
 import Register from './Register.vue'
 import Swap from './Swap.vue'
+import Modal from './Modal.vue'
 
 export {
-  Login, Purchase, Register, Swap
+  Login, Purchase, Register, Swap, Modal
 }
